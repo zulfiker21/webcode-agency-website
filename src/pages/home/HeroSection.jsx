69f184e2ce4569bd9bc2f1400be320fe84c65608
zulfiker21 <zulfikerhossain2023@gmail.com>
2025-03-03 23:53:57 +0600

    update

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -41,9 +41,7 @@ const HeroSection = () => {
                     showModal && (<div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
                         <div className='bg-white p-5 rounded-lg shadow-lg relative max-w-xl w-full'>
                             <button onClick={closeModal} className='absolute top-3 right-3 text-white text-lg hover:text-white bg-black px-2 rounded-full'>&times;</button>
-                            <iframe 
-                            className='w-full aspect-video'
-                            width="560" height="315" src="https://www.youtube.com/embed/sqyuaO_AnwQ?si=_io8D8Mamwq8gMl4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            <iframe className='w-full aspect-video' width="560" height="315" src="https://www.youtube.com/embed/SqcY0GlETPk?si=CEU4xlrgyQRRAnpi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                         </div>
                     </div>)
                 }
